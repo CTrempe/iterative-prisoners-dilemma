01de06dd3ba82bcfc2ae1f1785831f353ffa5535
CTrempe <trempe_connor@asd.lan>
2017-03-16 12:41:54 -0500

    Team 3 algorithm
    Always betrays. Colludes every fith turn

--- team2.py
+++ team2.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Stompe' # Only 10 chars displayed.
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -17,13 +17,7 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    turn = 0
-    for x in their_history:
-        turn += 1
-    if turn % 5 == 0 and turn != 0:
-        return 'c'
-    else:
-        return 'b'
+
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -32,7 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    
+    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
